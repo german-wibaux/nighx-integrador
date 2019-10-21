65c74e5a460e02a32ef0245d8e050b3eff81e48c
PabloSantaMaria <pabloj.santamaria@gmail.com>
2019-10-21 15:21:33 -0300

    ajax

---
+++ ajax/ajax.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  function render(path) {
+    fetch(path).then(function(response) {
+      if (response.ok) {
+        response.text().then(function(t) {
+          const container = document.getElementById('ajax');
+          container.innerHTML = t;
+        });
+      } else {
+        document.querySelector('#ajax').innerHTML = '<h1>No encontró el archivo</h1>';
+      }
+    })
+    .catch(function(response) {
+      console.log(response);
+      document.querySelector('#ajax').innerHTML = '<h1>Error - Connection Failed!</h1>';
+    });
+  }
+
+  const htmlButton = document.getElementById('html');
+  htmlButton.onclick = function(event) {
+    event.preventDefault;
+    render('test.html');
+  };
+});
