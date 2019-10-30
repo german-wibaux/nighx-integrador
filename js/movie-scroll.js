921efb56398622756d9cc1f422df97eda2736d48
PabloSantaMaria <pabloj.santamaria@gmail.com>
2019-10-30 18:11:45 -0300

    home- logueado mobile falta nav

--- js/movie-scroll.js
+++ js/movie-scroll.js
@@ -4,11 +4,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const recomendaciones = document.getElementById('recomendaciones');
 
     const laContinuar = document.getElementById('laContinuar');
+    const laContinuarM = document.getElementById('laContinuarM');
     const raContinuar = document.getElementById('raContinuar');
+    const raContinuarM = document.getElementById('raContinuarM');
+
     const laSugerencias = document.getElementById('laSugerencias');
+    const laSugerenciasM = document.getElementById('laSugerenciasM');
     const raSugerencias = document.getElementById('raSugerencias');
+    const raSugerenciasM = document.getElementById('raSugerenciasM');
+
     const laRecomendaciones = document.getElementById('laRecomendaciones');
+    const laRecomendacionesM = document.getElementById('laRecomendacionesM');
     const raRecomendaciones = document.getElementById('raRecomendaciones');
+    const raRecomendacionesM = document.getElementById('raRecomendacionesM');
 
     function scroll(tira, lArrow, rArrow, direction) {
         switch (direction) {
@@ -37,25 +45,53 @@ window.addEventListener('DOMContentLoaded', (event) => {
         event.preventDefault;
         scroll(continuar, laContinuar, raContinuar, 'left');
     };
+    document.getElementById('lContinuarM').onclick = function (event) {
+        event.preventDefault;
+        scroll(continuar, laContinuarM, raContinuarM, 'left');
+    };
     document.getElementById('rContinuar').onclick = function (event) {
         event.preventDefault;
         scroll(continuar, laContinuar, raContinuar, 'right');
     };
+    document.getElementById('rContinuarM').onclick = function (event) {
+        event.preventDefault;
+        scroll(continuar, laContinuarM, raContinuarM, 'right');
+    };
+
+    
     document.getElementById('lSugerencias').onclick = function (event) {
         event.preventDefault;
         scroll(sugerencias, laSugerencias, raSugerencias, 'left');
+    };
+    document.getElementById('lSugerenciasM').onclick = function (event) {
+        event.preventDefault;
+        scroll(sugerencias, laSugerenciasM, raSugerenciasM, 'left');
     };
     document.getElementById('rSugerencias').onclick = function (event) {
         event.preventDefault;
         scroll(sugerencias, laSugerencias, raSugerencias, 'right');
     };
+    document.getElementById('rSugerenciasM').onclick = function (event) {
+        event.preventDefault;
+        scroll(sugerencias, laSugerenciasM, raSugerenciasM, 'right');
+    };
+
+
     document.getElementById('lRecomendaciones').onclick = function (event) {
         event.preventDefault;
         scroll(recomendaciones, laRecomendaciones, raRecomendaciones, 'left');
     };
+    document.getElementById('lRecomendacionesM').onclick = function (event) {
+        event.preventDefault;
+        scroll(recomendaciones, laRecomendacionesM, raRecomendacionesM, 'left');
+    };
     document.getElementById('rRecomendaciones').onclick = function (event) {
         event.preventDefault;
         scroll(recomendaciones, laRecomendaciones, raRecomendaciones, 'right');
+    };
+    document.getElementById('rRecomendacionesM').onclick = function (event) {
+        event.preventDefault;
+        scroll(recomendaciones, laRecomendacionesM, raRecomendacionesM, 'right');
     };
 
 });
